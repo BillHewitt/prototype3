ec0106ae00c16eabba8b0a252e25661685531c8e
Bill Hewitt <billhewitt22@googlemail.com>
2019-03-23 12:02:13 +0000

    ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require_tree .
 //= require_tree .
